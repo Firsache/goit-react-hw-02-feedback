d8d1c9b50a8a7fd11a03618db52782f6a735088a
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-09 13:31:21 +0200

    added FeedbackOptions folder

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,12 @@
+import { Section } from 'components';
+
+export function FeedbackOptions() {
+  return (
+    <Section>
+      <h2>Please leave your feedback</h2>
+      <button>Good</button>
+      <button>Neutral</button>
+      <button>Bad</button>
+    </Section>
+  );
+}
