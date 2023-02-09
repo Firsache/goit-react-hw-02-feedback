ce846c2449a4a1047cc8db1cba3903a7ee6dea3c
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-09 13:30:49 +0200

    Create Section.jsx

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+
+export function Section() {
+  return (
+    <section>
+      <title></title>
+    </section>
+  );
+}
+
+Section.propTypes = {
+  title: PropTypes.string,
+};
