d19189ea6b3d5711a533642d7779ed99e89477bb
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-10 12:22:09 +0200

    Create colorPicker.js

---
+++ src/components/colorPicker.js
@@ -0,0 +1,12 @@
+export const colorPicker = option => {
+  switch (option) {
+    case 'good':
+      return 'hsl(127deg 65% 50% / 70%)';
+    case 'neutral':
+      return 'hsl(212deg 100% 47% / 70%)';
+    case 'bad':
+      return 'hsl(4deg 78% 53% / 70%)';
+    default:
+      return '#303030';
+  }
+};
